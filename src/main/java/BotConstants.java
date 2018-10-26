1bdbe7b30e22a5860875be8007d2ec9647a1263a
Luke Garrigan <lukegarrigan8@gmail.com>
2018-10-26 22:56:17 +0100

    Update BotConstants.java

--- src/main/java/BotConstants.java
+++ src/main/java/BotConstants.java
@@ -1,8 +1,7 @@
 public class BotConstants {
 
 
-    public static final String BOT_ID = "NTA0NzIwOTA1NDM2NjU5NzEy.DrJKfw.C544zzxQxI8bcNdoBORZCeolzrQ";
-    public static final String FRIENDLY_BOT_ID = "NTA1NDU5NDMzMjAwMTU2Njgy.DrT5cA.E2BuvPUnDp-QNrfv7sqY0fWG9VA";
+ 
     private BotConstants() {
 
 
